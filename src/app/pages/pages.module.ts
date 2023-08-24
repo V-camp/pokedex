dfee8bf905b75d65ddd8dad66519f0695e380d21
VCamp <vini.camp1907@gmail.com>
2023-08-24 08:57:33 -0300

    Projeto pokedex

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Pages
+import { HomeComponent } from './home/home.component';
+
+//Module Routing
+import { RoutingModule } from './routing.module';
+import { DetailsComponent } from './details/details.component';
+import { SharedModule } from './../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  exports: [
+    SharedModule
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
